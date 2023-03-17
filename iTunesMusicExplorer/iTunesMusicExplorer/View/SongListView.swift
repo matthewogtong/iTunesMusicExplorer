ad124a33e1e158b77af331aac38c73bc86106526
Matthew Ogtong <ogtongm@gmail.com>
2023-03-16 20:34:24 -0400

    Implement grouping by kind in SongListView

--- iTunesMusicExplorer/iTunesMusicExplorer/View/SongListView.swift
+++ iTunesMusicExplorer/iTunesMusicExplorer/View/SongListView.swift
@@ -13,23 +13,32 @@ struct SongListView: View {
 
     @StateObject private var songListViewModel = SongListViewModel()
 
+    private func groupedSongs() -> [String: [Song]] {
+        Dictionary(grouping: songListViewModel.songs, by: { $0.kind ?? "" })
+    }
+
     var body: some View {
         NavigationView {
-
             if songListViewModel.isLoading {
                 ProgressView()
             } else {
-                List(songListViewModel.songs) { song in
-                    SongRow(song: song)
+                List {
+                    ForEach(groupedSongs().keys.sorted(), id: \.self) { key in
+                        Section(header: Text(key)) {
+                            ForEach(groupedSongs()[key] ?? []) { song in
+                                SongRow(song: song)
+                            }
+                        }
+                    }
                 }
             }
         }
         .onAppear(perform: songListViewModel.fetchRequest)
     }
-}
 
-struct SongListView_Previews: PreviewProvider {
-    static var previews: some View {
-        SongListView()
+    struct SongListView_Previews: PreviewProvider {
+        static var previews: some View {
+            SongListView()
+        }
     }
 }
