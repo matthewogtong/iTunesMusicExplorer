0de0bf3f85d4705ea08656912c878b9a00e2d560
Matthew Ogtong <ogtongm@gmail.com>
2023-03-17 00:24:41 -0400

    Adjust baseUrl to properly implement search function

--- iTunesMusicExplorer/iTunesMusicExplorer/Constants/APIConstants.swift
+++ iTunesMusicExplorer/iTunesMusicExplorer/Constants/APIConstants.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 enum APIConstants {
-    static let baseURL = "http://itunes.apple.com/search?term=beyonce&limit=25&media=music&entity=musicArtist,musicTrack,album,mix,song"
+    static let baseURL = "http://itunes.apple.com/search"
 }
